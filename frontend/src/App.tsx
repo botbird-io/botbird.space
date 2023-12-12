7506e1a05cb2cf520a637e16be29c60be38d1bd9
sanket-sapate <sanketsapatevnit@gmail.com>
2023-12-12 15:47:14 +0530

    checking changes 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img  className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React changes</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
